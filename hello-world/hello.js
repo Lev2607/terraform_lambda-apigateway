e8898a2b94c4da35d81b36ae8a6479c8c9cf2dd0
levent2607 <levent.o@web.de>
2024-01-16 10:23:41 +0100

    first commit

--- hello-world/hello.js
+++ hello-world/hello.js
@@ -3,7 +3,7 @@ hello.js
 /**
  * Copyright (c) HashiCorp, Inc.
  * SPDX-License-Identifier: MPL-2.0
- */
+ 
 
 // Lambda function code
 
@@ -24,4 +24,42 @@ module.exports.handler = async (event) => {
       message: responseMessage,
     }),
   }
-}
+} */
+
+exports.handler = async (event) => {
+  console.log('Event: ', event);
+
+  let response;
+  const resource = event.resource;
+  const httpMethod = event.httpMethod;
+
+  if (resource === "/say-hi" && httpMethod === "GET") {
+    response = {
+      statusCode: 200,
+      body: JSON.stringify({ from: "/say-hi" })
+    };
+  } else if (resource === "/save" && httpMethod === "POST") {
+    response = {
+      statusCode: 200,
+      body: JSON.stringify({ from: "/save" })
+    };
+  } else if (resource === "/remove" && httpMethod === "DELETE") {
+    response = {
+      statusCode: 200,
+      body: JSON.stringify({ from: "/remove" })
+    };
+  } else {
+    response = {
+      statusCode: 404,
+      body: JSON.stringify({ message: "Resource not found or method not supported" })
+    };
+  }
+
+  return {
+    statusCode: response.statusCode,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: response.body
+  };
+};

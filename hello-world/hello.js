5a43b8d59e953c5eabe722c24014c78b062ffdb3
levent2607 <levent.o@web.de>
2024-01-16 11:25:21 +0100

    first commit

--- hello-world/hello.js
+++ hello-world/hello.js
@@ -1,29 +1,3 @@
-/**
- * Copyright (c) HashiCorp, Inc.
- * SPDX-License-Identifier: MPL-2.0
- 
-
-// Lambda function code
-
-module.exports.handler = async (event) => {
-  console.log('Event: ', event);
-  let responseMessage = 'Hello, World!';
-
-  if (event.queryStringParameters && event.queryStringParameters['Name']) {
-    responseMessage = 'Hello, ' + event.queryStringParameters['Name'] + '!';
-  }
-
-  return {
-    statusCode: 200,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      message: responseMessage,
-    }),
-  }
-} **/
-
 exports.handler = async (event) => {
   console.log('Event: ', event);
 
